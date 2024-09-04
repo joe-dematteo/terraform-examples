1a8da4a626b95a80ce3cd561cc17ee9ee9878eb5
joe-dematteo <joedematteo9@gmail.com>
2024-09-04 16:31:35 -0400

    working fargate

--- ecs-rolling/modules/ecs/main.tf
+++ ecs-rolling/modules/ecs/main.tf
@@ -13,23 +13,17 @@ module "ecs_cluster" {
 
   cluster_name = var.cluster_name
 
-  # Capacity provider - autoscaling groups
-  default_capacity_provider_use_fargate = false
-  autoscaling_capacity_providers = {
-    # Spot instances
-    ex_2 = {
-      auto_scaling_group_arn         = module.autoscaling["ex_2"].autoscaling_group_arn
-      managed_termination_protection = "ENABLED"
-
-      managed_scaling = {
-        maximum_scaling_step_size = 15
-        minimum_scaling_step_size = 5
-        status                    = "ENABLED"
-        target_capacity           = 90
-      }
-
+  # Capacity provider
+  fargate_capacity_providers = {
+    FARGATE = {
       default_capacity_provider_strategy = {
-        weight = 40
+        weight = 50
+        base   = 20
+      }
+    }
+    FARGATE_SPOT = {
+      default_capacity_provider_strategy = {
+        weight = 50
       }
     }
   }
@@ -48,45 +42,27 @@ module "ecs_service" {
   name        = var.cluster_name
   cluster_arn = module.ecs_cluster.arn
 
-  # Task Definition
-  requires_compatibilities = ["EC2"]
-  capacity_provider_strategy = {
-    # On-demand instances
-    # ex_1 = {
-    #   capacity_provider = module.ecs_cluster.autoscaling_capacity_providers["ex_1"].name
-    #   weight            = 1
-    #   base              = 1
-    # }
-  }
+  cpu = 256
+  memory = 512
 
-  volume = {
-    my-vol = {}
-  }
-
+  # Enables ECS Exec
+  enable_execute_command = true
 
   # Container definition(s)
   container_definitions = {
     (local.container_name) = {
-      # NEED IMAGE HERE, was having issues with pulling image on tasks..? Try to push nginx image to a public ecr and try that
-      image  = "896644348821.dkr.ecr.us-east-1.amazonaws.com/joeandjoe-temp:overflow-marketing-flow-web-2-9afa7b764530b0622a6126876754643a948eeb90"
+      image  = "896644348821.dkr.ecr.us-east-1.amazonaws.com/joeandjoe-temp:overflow-marketing-booking-73af0a8d03ce585ed8a7da9dfbc44b4c000b7103"
       cpu    = 256
       memory = 512
       port_mappings = [
         {
           name          = local.container_name
           containerPort = local.container_port
+          hostPort      = local.container_port
           protocol      = "tcp"
         }
       ]
 
-      mount_points = [
-        {
-          sourceVolume  = "my-vol",
-          containerPath = "/var/www/my-vol"
-        }
-      ]
-
-      entry_point = ["/usr/sbin/apache2", "-D", "FOREGROUND"]
 
       # Example image used requires access to write to root filesystem
       readonly_root_filesystem = false
@@ -99,6 +75,18 @@ module "ecs_service" {
       log_configuration = {
         logDriver = "awslogs"
       }
+    }
+  }
+
+  service_connect_configuration = {
+    namespace = aws_service_discovery_http_namespace.this.arn
+    service = {
+      client_alias = {
+        port     = local.container_port
+        dns_name = local.container_name
+      }
+      port_name      = local.container_name
+      discovery_name = local.container_name
     }
   }
 
@@ -129,12 +117,22 @@ module "ecs_service" {
     }
   }
 
+  service_tags = {
+    "ServiceTag" = "Tag on service level"
+  }
+
   tags = var.tags
 }
 
 ################################################################################
 # Supporting Resources
 ################################################################################
+
+resource "aws_service_discovery_http_namespace" "this" {
+  name        = var.cluster_name
+  description = "CloudMap namespace for ${var.cluster_name}"
+  tags        = var.tags
+}
 
 # https://docs.aws.amazon.com/AmazonECS/latest/developerguide/ecs-optimized_AMI.html#ecs-optimized-ami-linux
 data "aws_ssm_parameter" "ecs_optimized_ami" {
@@ -213,126 +211,6 @@ module "alb" {
       create_attachment = false
     }
   }
-
-  tags = var.tags
-}
-
-module "autoscaling" {
-  source  = "terraform-aws-modules/autoscaling/aws"
-  version = "~> 6.5"
-
-  for_each = {
-    # On-demand instances
-    # ex_1 = {
-    #   instance_type              = "t3.large"
-    #   use_mixed_instances_policy = false
-    #   mixed_instances_policy     = {}
-    #   user_data                  = <<-EOT
-    #     #!/bin/bash
-
-    #     cat <<'EOF' >> /etc/ecs/ecs.config
-    #     ECS_CLUSTER=${var.cluster_name}
-    #     ECS_LOGLEVEL=debug
-    #     ECS_CONTAINER_INSTANCE_TAGS=${jsonencode(var.tags)}
-    #     ECS_ENABLE_TASK_IAM_ROLE=true
-    #     EOF
-    #   EOT
-    # }
-    # Spot instances
-    ex_2 = {
-      instance_type              = "t3.medium"
-      use_mixed_instances_policy = true
-      mixed_instances_policy = {
-        instances_distribution = {
-          on_demand_base_capacity                  = 0
-          on_demand_percentage_above_base_capacity = 0
-          spot_allocation_strategy                 = "price-capacity-optimized"
-        }
-
-        override = [
-          {
-            instance_type     = "t3.medium"
-            weighted_capacity = "2"
-          },
-          {
-            instance_type     = "t3.small"
-            weighted_capacity = "1"
-          },
-        ]
-      }
-      user_data = <<-EOT
-        #!/bin/bash
-
-        cat <<'EOF' >> /etc/ecs/ecs.config
-        ECS_CLUSTER=${var.cluster_name}
-        ECS_LOGLEVEL=debug
-        ECS_CONTAINER_INSTANCE_TAGS=${jsonencode(var.tags)}
-        ECS_ENABLE_TASK_IAM_ROLE=true
-        ECS_ENABLE_SPOT_INSTANCE_DRAINING=true
-        EOF
-      EOT
-    }
-  }
-
-  name = "${var.cluster_name}-${each.key}"
-
-  image_id      = jsondecode(data.aws_ssm_parameter.ecs_optimized_ami.value)["image_id"]
-  instance_type = each.value.instance_type
-
-  security_groups                 = [module.autoscaling_sg.security_group_id]
-  user_data                       = base64encode(each.value.user_data)
-  ignore_desired_capacity_changes = true
-
-  create_iam_instance_profile = true
-  iam_role_name               = var.cluster_name
-  iam_role_description        = "ECS role for ${var.cluster_name}"
-  iam_role_policies = {
-    AmazonEC2ContainerServiceforEC2Role = "arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceforEC2Role"
-    AmazonSSMManagedInstanceCore        = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
-  }
-
-  vpc_zone_identifier = module.vpc.private_subnets
-  health_check_type   = "EC2"
-  min_size            = 1
-  max_size            = 5
-  desired_capacity    = 2
-
-  # https://github.com/hashicorp/terraform-provider-aws/issues/12582
-  autoscaling_group_tags = {
-    AmazonECSManaged = true
-  }
-
-  # Required for  managed_termination_protection = "ENABLED"
-  protect_from_scale_in = true
-
-  # Spot instances
-  use_mixed_instances_policy = each.value.use_mixed_instances_policy
-  mixed_instances_policy     = each.value.mixed_instances_policy
-
-  tags = var.tags
-}
-
-module "autoscaling_sg" {
-  source  = "terraform-aws-modules/security-group/aws"
-  version = "~> 5.0"
-
-  name        = var.cluster_name
-  description = "Autoscaling group security group"
-  vpc_id      = module.vpc.vpc_id
-
-  computed_ingress_with_source_security_group_id = [
-    {
-      rule                     = "http-80-tcp"
-      source_security_group_id = module.alb.security_group_id
-    },
-    {
-      rule                     = "https-443-tcp"
-      source_security_group_id = module.alb.security_group_id
-    }
-  ]
-  number_of_computed_ingress_with_source_security_group_id = 1
-
-  egress_rules = ["all-all"]
 
   tags = var.tags
 }
